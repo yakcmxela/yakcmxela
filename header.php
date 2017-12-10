8a6e61a1c4337c9130f5232bac7aee676b5f5f3f
Alex McKay <alex@boldcoastcreative.com>
2017-12-10 12:51:36 -0500

    first commit

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 
-// The footer template. No Frills Oil.
+// Header
 // Author: Alex McKay 2017
 // Bold Coast Creative www.boldcoastcreative.com
 
@@ -31,14 +31,11 @@
 		<div class="skills">
 			<img src="<?php echo get_template_directory_uri(); ?>/svg/skills.svg">
 		</div>
-		<div class="experience">
-			<img src="<?php echo get_template_directory_uri(); ?>/svg/work.svg">
-		</div>
 		<div class="about">
 			<img src="<?php echo get_template_directory_uri(); ?>/svg/about.svg">
 		</div>
 		<div class="contact" >
-			<img src="<?php echo get_template_directory_uri(); ?>/svg/work.svg">
+			<img src="<?php echo get_template_directory_uri(); ?>/svg/contact.svg">
 		</div>
 		
 	</div>
